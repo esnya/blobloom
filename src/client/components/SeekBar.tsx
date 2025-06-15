9b9cdae43bcd9e74e0ab5684701d96838e121ef6
esnya <2088693+esnya@users.noreply.github.com>
2025-06-15 18:29:29 +0900

    feat(seekbar): :sparkles: make component controlled

--- src/client/components/SeekBar.tsx
+++ src/client/components/SeekBar.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 export interface SeekBarProps {
+  value: number;
   onInput: (value: number) => void;
 }
 
-export const SeekBar = React.forwardRef<HTMLInputElement, SeekBarProps>(
-  ({ onInput }, ref) => (
+export function SeekBar({ value, onInput }: SeekBarProps): React.JSX.Element {
+  return (
     <input
       type="range"
-      ref={ref}
+      value={value}
       onInput={(e) => onInput(Number((e.target as HTMLInputElement).value))}
     />
-  ),
-);
+  );
+}

5b1bc13e1480dbd2689eef52fc599432c05bab7c
esnya <2088693+esnya@users.noreply.github.com>
2025-06-15 15:48:28 +0900

    chore(lint): 🐛 migrate ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,28 @@
-import { createRequire } from 'node:module';
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 
-const require = createRequire(import.meta.url);
-const eslintrc = require('./.eslintrc.json');
+const eslintrc = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    '@typescript-eslint/no-unused-vars': ['error']
+  }
+};
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,

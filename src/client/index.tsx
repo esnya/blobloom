cdd12617c0a3502db22df15257878b2a984516fa
esnya <2088693+esnya@users.noreply.github.com>
2025-06-15 15:44:05 +0900

    chore(deps): :arrow_up: update packages

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import type { JSX } from 'react';
 import ReactDOM from 'react-dom/client';
 import { fetchCommits, fetchLineCounts } from './api.js';
 import { createPlayer } from './player.js';

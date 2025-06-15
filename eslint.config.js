8ee4a9cf167e481c9e981d545a56c1ab602b9028
esnya <2088693+esnya@users.noreply.github.com>
2025-06-15 19:48:20 +0900

    feat(lint): 🎨 enhance react checks with warnings

--- eslint.config.js
+++ eslint.config.js
@@ -13,16 +13,33 @@ const eslintrc = {
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
   ],
   rules: {
     semi: ['error', 'always'],
     quotes: ['error', 'single'],
-    '@typescript-eslint/no-unused-vars': ['error']
+    '@typescript-eslint/no-unused-vars': ['error'],
+    // TODO: change to 'error' after resolving legacy issues
+    'react/display-name': 'warn',
+    // TODO: change to 'error' after resolving legacy issues
+    'react/jsx-uses-react': 'warn',
+    // TODO: change to 'error' after resolving legacy issues
+    'react/react-in-jsx-scope': 'warn',
+    // TODO: change to 'error' after verifying hook usage
+    'react-hooks/rules-of-hooks': 'warn',
+    // TODO: change to 'error' after verifying dependencies
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 };
 

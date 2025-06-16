cbad8ee9adcb0a4dfb195cc60ec335b7e586b8f0
esnya <2088693+esnya@users.noreply.github.com>
2025-06-16 22:50:02 +0900

    feat(char-effects): :sparkles: animate scatter on unmount

--- src/__tests__/components/fileCircle.radius.test.tsx
+++ src/__tests__/components/fileCircle.radius.test.tsx
@@ -2,13 +2,16 @@
 import React from 'react';
 import { render, act } from '@testing-library/react';
 import { PhysicsProvider } from '../../client/hooks/useEngine';
+import { CharEffectsProvider } from '../../client/hooks/useGlobalCharEffects';
 import { FileCircle } from '../../client/components/FileCircle';
 
 jest.useFakeTimers();
 
 describe('FileCircle radius effect', () => {
   const Wrapper = ({ children }: { children: React.ReactNode }) => (
-    <PhysicsProvider bounds={{ width: 100, height: 100 }}>{children}</PhysicsProvider>
+    <PhysicsProvider bounds={{ width: 100, height: 100 }}>
+      <CharEffectsProvider>{children}</CharEffectsProvider>
+    </PhysicsProvider>
   );
 
   it('does not exceed update depth when radius changes', () => {

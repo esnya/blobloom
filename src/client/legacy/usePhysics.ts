ac5192a0537fb1aad89b92fe58dbbee4f81846ed
esnya <2088693+esnya@users.noreply.github.com>
2025-06-16 15:38:50 +0900

    chore(cleanup): 🗑 remove unused legacy modules

--- src/client/legacy/usePhysics.ts
+++
@@ -1,8 +0,0 @@
-import { useMemo } from 'react';
-import * as Physics from '../physics';
-
-/**
- * Provides the simple physics API through a hook so components don't
- * import the library directly.
- */
-export const usePhysics = () => useMemo(() => Physics, []);

aa60b449d39ec4db37d115e71640811e445e0573
esnya <2088693+esnya@users.noreply.github.com>
2025-06-17 00:06:29 +0900

    refactor(server): :recycle: leverage diff library for line counts

--- src/server/line-counts.ts
+++ src/server/line-counts.ts
@@ -1,6 +1,7 @@
 import * as git from 'isomorphic-git';
 import fs from 'fs';
 import { minimatch } from 'minimatch';
+import diff from 'diff-sequences';
 
 const isBinary = (buf: Buffer): boolean => {
   const len = Math.min(buf.length, 1000);
@@ -21,16 +22,16 @@ const diffCounts = (
   a: string[],
   b: string[],
 ): { added: number; removed: number } => {
-  const m = a.length;
-  const n = b.length;
-  const dp: number[][] = Array.from({ length: m + 1 }, () => Array<number>(n + 1).fill(0));
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      dp[i]![j] = a[i - 1] === b[j - 1] ? dp[i - 1]![j - 1]! + 1 : Math.max(dp[i - 1]![j]!, dp[i]![j - 1]!);
-    }
-  }
-  const lcs = dp[m]![n]!;
-  return { added: n - lcs, removed: m - lcs };
+  let common = 0;
+  diff(
+    a.length,
+    b.length,
+    (ai, bi) => a[ai] === b[bi],
+    (nCommon) => {
+      common += nCommon;
+    },
+  );
+  return { added: b.length - common, removed: a.length - common };
 };
 
 export const getLineCounts = async ({

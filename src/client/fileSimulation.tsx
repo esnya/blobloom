2195ffc57859a89e6aafd11216cce4a9e0e6eb24
esnya <2088693+esnya@users.noreply.github.com>
2025-06-16 20:41:46 +0900

    Revert "feat(file-circle): 🌟 scatter on unmount"
    This reverts commit e9d901c67c57c0ac3c7104236200d57072024488.

--- src/client/fileSimulation.tsx
+++ src/client/fileSimulation.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { flushSync } from 'react-dom';
 import { FileCircleList } from './components/FileCircleSimulation';
-import { CharEffectsProvider } from './hooks/useGlobalCharEffects';
 import { PhysicsProvider } from './hooks/useEngine';
 import * as Physics from './physics';
 
@@ -36,13 +35,11 @@ export const createFileSimulation = (
     flushSync(() =>
       root.render(
         <PhysicsProvider bounds={{ width, height }} engine={engine}>
-          <CharEffectsProvider>
-            <FileCircleList
-              data={currentData}
-              bounds={{ width, height }}
-              {...(opts.linear !== undefined ? { linear: opts.linear } : {})}
-            />
-          </CharEffectsProvider>
+          <FileCircleList
+            data={currentData}
+            bounds={{ width, height }}
+            {...(opts.linear !== undefined ? { linear: opts.linear } : {})}
+          />
         </PhysicsProvider>,
       ),
     );

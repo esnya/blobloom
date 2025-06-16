7d8755e13d4fc328ac329363e85fc7cf22ea1434
esnya <2088693+esnya@users.noreply.github.com>
2025-06-16 23:57:42 +0900

    fix(dev-server): :bug: enable WebSocket in Vite plugin

--- src/server/vite-express.ts
+++ src/server/vite-express.ts
@@ -1,6 +1,8 @@
 import type { Plugin } from 'vite';
 import type { NextHandleFunction } from 'connect';
 import { apiMiddleware } from './api-middleware';
+import { setupLineCountWs } from './ws';
+import type { Server } from 'http';
 import express from 'express';
 
 export default function viteExpress(): Plugin {
@@ -10,6 +12,9 @@ export default function viteExpress(): Plugin {
       const app = express();
       app.use(apiMiddleware);
       server.middlewares.use(app as unknown as NextHandleFunction);
+      if (server.httpServer) {
+        setupLineCountWs(app, server.httpServer as unknown as Server);
+      }
     },
   };
 }

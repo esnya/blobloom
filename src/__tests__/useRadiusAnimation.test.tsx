fad67304c8301ddef30c430dcf9b58c08a549f9c
esnya <2088693+esnya@users.noreply.github.com>
2025-06-17 02:02:58 +0900

    test(hooks): :white_check_mark: cover animation hooks

--- src/__tests__/useRadiusAnimation.test.tsx
+++ src/__tests__/useRadiusAnimation.test.tsx
@@ -1,10 +1,10 @@
 /** @jest-environment jsdom */
 import { renderHook, act } from '@testing-library/react';
-import { useAnimatedNumber } from '../client/hooks/useAnimatedNumber';
+import { useRadiusAnimation } from '../client/hooks/useRadiusAnimation';
 
 jest.useFakeTimers();
 
-describe('useAnimatedNumber', () => {
+describe('useRadiusAnimation', () => {
   const originalRaf = global.requestAnimationFrame;
   let now = 0;
 
@@ -26,7 +26,7 @@ describe('useAnimatedNumber', () => {
   });
 
   it('eases to the target within duration', () => {
-    const { result } = renderHook(() => useAnimatedNumber(10, { duration: 100 }));
+    const { result } = renderHook(() => useRadiusAnimation(10, 100));
 
     act(() => {
       result.current[1](20);
@@ -44,7 +44,7 @@ describe('useAnimatedNumber', () => {
   });
 
   it('returns a stable animate function', () => {
-    const { result } = renderHook(() => useAnimatedNumber(10, { duration: 100 }));
+    const { result } = renderHook(() => useRadiusAnimation(10, 100));
     const animate = result.current[1];
 
     act(() => {

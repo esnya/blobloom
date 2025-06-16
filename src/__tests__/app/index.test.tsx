7cf162f4fe232e07e910b7c215d1475f6999295a
esnya <2088693+esnya@users.noreply.github.com>
2025-06-17 02:55:29 +0900

    test(app): :recycle: unify index mounting tests

--- src/__tests__/app/index.test.tsx
+++ src/__tests__/app/index.test.tsx
@@ -1,15 +1,33 @@
 /** @jest-environment jsdom */
+import { waitFor } from '@testing-library/react';
+import { setupAppTest } from '../helpers/app';
 
 describe('index.tsx', () => {
+  let restore: () => void;
+
   beforeEach(() => {
     jest.resetModules();
-    document.body.innerHTML = '<div id="root"></div>';
+    restore = setupAppTest();
+  });
+
+  afterEach(() => {
+    restore();
   });
 
   it('mounts the app', async () => {
-    const createRoot = jest.fn(() => ({ render: jest.fn() }));
-    jest.doMock('react-dom/client', () => ({ createRoot }));
+    const root = document.getElementById('root') as HTMLElement;
+    const actual = await import('react-dom/client');
+    const spy = jest.fn(actual.createRoot);
+    jest.doMock('react-dom/client', () => ({
+      ...actual,
+      createRoot: (...args: Parameters<typeof actual.createRoot>) => {
+        const instance = actual.createRoot(...args);
+        spy(...args);
+        return instance;
+      },
+    }));
     await import('../../client/index');
-    expect(createRoot).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
+    await waitFor(() => expect(root.firstChild).toBeTruthy());
   });
 });

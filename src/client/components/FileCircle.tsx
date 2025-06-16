b2ed999b9226f63390812bb866c64a71675422c9
esnya <2088693+esnya@users.noreply.github.com>
2025-06-17 02:59:07 +0900

    fix(file-circle): 🌀 add friction for rotation

--- src/client/components/FileCircle.tsx
+++ src/client/components/FileCircle.tsx
@@ -76,6 +76,7 @@ export const FileCircle = React.forwardRef<HTMLDivElement, FileCircleProps>(
   const { body, setRadius: setBodyRadius } = useBody({
     radius,
     restitution: 0.5,
+    friction: 0.5,
     frictionAir: 0.001,
     onUpdate: forceUpdate,
   });

c3045e35a1ddd5c2d7f94380d2fe115eb4ee0c37
esnya <2088693+esnya@users.noreply.github.com>
2025-06-17 00:19:17 +0900

    refactor(file-circle): 🔥 remove unused radius variable

--- src/client/components/FileCircle.tsx
+++ src/client/components/FileCircle.tsx
@@ -118,7 +118,6 @@ export const FileCircle = React.forwardRef<HTMLDivElement, FileCircleProps>(
           background: color,
           willChange: 'transform',
           transform: `translate3d(${body.position.x - currentRadius}px, ${body.position.y - currentRadius}px, 0) rotate(${body.angle}rad)`,
-          '--r': `${currentRadius}px`,
         } as React.CSSProperties}
       >
         <FileCircleContent

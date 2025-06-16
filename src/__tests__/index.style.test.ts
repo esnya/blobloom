0d573a6d979e4939e248935b7004346908c186a1
esnya <2088693+esnya@users.noreply.github.com>
2025-06-17 00:24:15 +0900

    fix(css): 🎨 ensure circle text spans full width

--- src/__tests__/index.style.test.ts
+++ src/__tests__/index.style.test.ts
@@ -7,6 +7,9 @@ describe('index.html style', () => {
     expect(html).toMatch(/\.file-circle .path {[^}]*top: 30%/);
     expect(html).toMatch(/\.file-circle .name {[^}]*top: 50%/);
     expect(html).toMatch(/\.file-circle .count {[^}]*top: 70%/);
+    expect(html).toMatch(/\.file-circle .path {[^}]*width: 90%/);
+    expect(html).toMatch(/\.file-circle .name {[^}]*width: 100%/);
+    expect(html).toMatch(/\.file-circle .count {[^}]*width: 90%/);
     expect(html).toMatch(/\.file-circle .path {[^}]*max-width: 90%/);
     expect(html).toMatch(/\.file-circle .name {[^}]*max-width: 100%/);
     expect(html).toMatch(/\.file-circle .count {[^}]*max-width: 90%/);

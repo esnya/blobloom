05893eb404ecf554c15993fbd39c2e5bf662e002
esnya <2088693+esnya@users.noreply.github.com>
2025-06-17 03:01:47 +0900

    refactor(hooks): :recycle: remove unused wrappers

--- src/client/hooks/useGlowControl.ts
+++
@@ -1,6 +0,0 @@
-import { useGlowAnimation } from './useGlowAnimation';
-
-export const useGlowControl = () => {
-  const [startGlow, glowProps] = useGlowAnimation();
-  return { startGlow, glowProps } as const;
-};
--- src/client/hooks/useRadiusAnimation.ts
+++
@@ -1,6 +0,0 @@
-import { useAnimatedNumber } from './useAnimatedNumber';
-
-export const useRadiusAnimation = (
-  initial: number,
-  duration = 300,
-) => useAnimatedNumber(initial, { duration });

44a321ff4f0e432e3c1e701faf847ed2d0299295
esnya <2088693+esnya@users.noreply.github.com>
2025-06-18 17:33:10 +0900

    style(hooks): 🎨 limit ref eslint overrides

--- src/client/hooks/useFileCircleRefs.ts
+++ src/client/hooks/useFileCircleRefs.ts
@@ -1,16 +1,15 @@
 import React, { useCallback } from 'react';
 
 export const useFileCircleRefs = () => {
-  /* eslint-disable no-restricted-syntax */
-  const refs = React.useRef(new Map<string, React.RefObject<HTMLDivElement | null>>());
-  /* eslint-enable no-restricted-syntax */
+  // eslint-disable-next-line no-restricted-syntax
+  const refs = React.useRef(
+    new Map<string, React.RefObject<HTMLDivElement | null>>()
+  );
 
   const getRef = useCallback((file: string): React.RefObject<HTMLDivElement | null> => {
     let ref = refs.current.get(file);
     if (ref === undefined) {
-      /* eslint-disable no-restricted-syntax */
-      ref = React.createRef<HTMLDivElement>();
-      /* eslint-enable no-restricted-syntax */
+      ref = React.createRef<HTMLDivElement>(); // eslint-disable-line no-restricted-syntax
       refs.current.set(file, ref);
     }
     return ref;
